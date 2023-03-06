3ce6e4f80411457ecf54d3e6db4ac7ba2bdf07e8
jh-santos <62848933+jh-santos@users.noreply.github.com>
2023-03-06 12:18:39 -0300

    finalizado modulo_10

--- components/modulo_10.php
+++ components/modulo_10.php
@@ -1,7 +1,7 @@
 <section class='modulo_10'>
     <div class='container-fluid'>
         <div class='row'>
-            <div class='col-12'>
+            <div class='col-12 px-0 px-md-3'>
                 <div class="bloco bloco-1">
                     <button class="titulo f-800 item item-1 ativo" grupo="1">Administração</button>
                     <button class="titulo f-800 item item-2" grupo="2">Diretoria</button>
@@ -9,90 +9,107 @@
                     <button class="titulo f-800 item item-4" grupo="4">Comitês</button>
                 </div>
                 <div class="bloco bloco-2">
-                    <div class="item item-1" grupo="1">
-                        <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
+                    <div class="item item-1 ativo" grupo="1">
+                        <div class="colab">
+                            <img loading="eager" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
+                            <h2 class="titulo-2 f-800">Nome do Colaborador</h2>
+                            <h3 class="sub-1">Nome do Cargo</h3>
+                        </div>
+                        <div class="colab">
+                            <img loading="eager" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
+                            <h2 class="titulo-2 f-800">Nome do Colaborador</h2>
+                            <h3 class="sub-1">Nome do Cargo</h3>
+                        </div>
+                        <div class="colab">
+                            <img loading="eager" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
+                            <h2 class="titulo-2 f-800">Nome do Colaborador</h2>
+                            <h3 class="sub-1">Nome do Cargo</h3>
+                        </div>
+                        <div class="colab">
+                            <img loading="eager" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
+                            <h2 class="titulo-2 f-800">Nome do Colaborador</h2>
+                            <h3 class="sub-1">Nome do Cargo</h3>
+                        </div>
+                        <div class="colab">
+                            <img loading="eager" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
+                            <h2 class="titulo-2 f-800">Nome do Colaborador</h2>
+                            <h3 class="sub-1">Nome do Cargo</h3>
+                        </div>
+                        <div class="colab">
+                            <img loading="eager" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
+                            <h2 class="titulo-2 f-800">Nome do Colaborador</h2>
+                            <h3 class="sub-1">Nome do Cargo</h3>
+                        </div>
                     </div>
-                    <div class="item item-1" grupo="1">
-                        <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
+                    <div class="item item-2 " grupo="2">
+                        <div class="colab">
+                            <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
+                            <h2 class="titulo-2 f-800">Nome do Colaborador</h2>
+                            <h3 class="sub-1">Nome do Cargo</h3>
+                        </div>
+                        <div class="colab">
+                            <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
+                        </div>
+                        <div class="colab">
+                            <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
+                        </div>
                     </div>
-                    <div class="item item-1" grupo="2">
-                        <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
+                    <div class="item item-3 " grupo="3">
+                        <div class="colab">
+                            <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
+                            <h2 class="titulo-2 f-800">Nome do Colaborador</h2>
+                            <h3 class="sub-1">Nome do Cargo</h3>
+                        </div>
+                        <div class="colab">
+                            <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
+                            <h2 class="titulo-2 f-800">Nome do Colaborador</h2>
+                            <h3 class="sub-1">Nome do Cargo</h3>
+                        </div>
+                        <div class="colab">
+                            <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
+                        </div>
+                        <div class="colab">
+                            <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
+                        </div>
                     </div>
-                    <div class="item item-1" grupo="1">
-                        <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
+                    <div class="item item-4 " grupo="4">
+                        <div class="colab">
+                            <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
+                            <h2 class="titulo-2 f-800">Nome do Colaborador</h2>
+                            <h3 class="sub-1">Nome do Cargo</h3>
+                        </div>
+                        <div class="colab">
+                            <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
+                            <h2 class="titulo-2 f-800">Nome do Colaborador</h2>
+                            <h3 class="sub-1">Nome do Cargo</h3>
+                        </div>
+                        <div class="colab">
+                            <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
+                            <h2 class="titulo-2 f-800">Nome do Colaborador</h2>
+                            <h3 class="sub-1">Nome do Cargo</h3>
+                        </div>
+                        <div class="colab">
+                            <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
+                            <h2 class="titulo-2 f-800">Nome do Colaborador</h2>
+                            <h3 class="sub-1">Nome do Cargo</h3>
+                        </div>
                     </div>
-                    <div class="item item-1" grupo="1">
-                        <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
-                    </div>
-                    <div class="item item-1" grupo="2">
-                        <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
-                    </div>
-
-                    <div class="item item-1" grupo="1">
-                        <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
-                    </div>
-                    <div class="item item-1" grupo="1">
-                        <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
-                    </div>
-                    <div class="item item-1" grupo="1">
-                        <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
-                    </div>
-                    <div class="item item-1" grupo="1">
-                        <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
-                    </div>
-                    <div class="item item-1" grupo="2">
-                        <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
-                    </div>
-                    <div class="item item-1" grupo="2">
-                        <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
-                    </div>
-                    <div class="item item-1" grupo="1">
-                        <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
-                    </div>
-                    <div class="item item-1" grupo="1">
-                        <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
-                    </div>
-                    <div class="item item-1" grupo="1">
-                        <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
-                    </div>
-                    <div class="item item-1" grupo="1">
-                        <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
-                    </div>
-                    <div class="item item-1" grupo="2">
-                        <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
-                    </div>
-                    <div class="item item-1" grupo="2">
-                        <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
-                    </div>
-                    <div class="item item-1" grupo="1">
-                        <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
-                    </div>
-                    <div class="item item-1" grupo="1">
-                        <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
-                    </div>
-                    <div class="item item-1" grupo="1">
-                        <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
-                    </div>
-                    <div class="item item-1" grupo="1">
-                        <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
-                    </div>
-                    <div class="item item-1" grupo="2">
-                        <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
-                    </div>
-                    <div class="item item-1" grupo="2">
-                        <img loading="lazy" class="img-fluid w-100" src="<?php bloginfo('template_url'); ?>/assets/img/modulo_10_1.webp" alt="">
-                    </div>
-                    <div class="item item-3" grupo="3"></div>
-                    <div class="item item-4" grupo="3"></div>
-                    <div class="item item-5" grupo="3"></div>
-                    <div class="item item-5" grupo="3"></div>
-                    <div class="item item-5" grupo="3"></div>
                 </div>
             </div>
         </div>
     </div>
 </section>
 <script async>
+    function resizeModulo10() {
+        var temp = $('.modulo_10 .bloco-2 .item.ativo').outerHeight();
+        console.log(temp)
+        if (temp) {
+            $('.modulo_10 .bloco-2').animate({
+                'height': temp
+            }, 100)
+        }
+        temp = 0;
+    }
     $(document).ready(function() {
         $('.modulo_10 .bloco-1 .item').on('click', function() {
             event.preventDefault();
@@ -101,15 +118,13 @@
             $('.modulo_10 .bloco-2 .item').removeClass('ativo');
             $(this).addClass('ativo');
             $('.modulo_10 .bloco-2 .item[grupo="' + temp + '"]').addClass('ativo');
-            // calculo da altura
-            var quant = $('.modulo_10 .bloco-2 .item.ativo').length;
-            // console.log(quant)
-            var resp = quant / 4;
-            console.log(resp)
+            resizeModulo10();
         })
-
     });
     window.addEventListener('load', function() {
-
+        resizeModulo10();
+        $(window).resize(function() {
+            resizeModulo10();
+        });
     });
 </script>
